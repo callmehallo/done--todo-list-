443f2e67062059cd0fddf35cb37297b30e4464a7
Andreas Vorobjev <vvorobjev@gmx.de>
2022-05-29 02:57:24 +0200

    deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faPlus,
@@ -48,8 +48,3 @@ root.render(
     </SectionProvider>
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
